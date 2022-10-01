7df73cfbf34b27629d9c20fc3724af07b3b4dab9
Luis Float <contact@luisfloat.com>
2022-09-30 23:44:39 -0300

    feat(main): export simple npm init module

---
+++ src/main/index.js
@@ -0,0 +1,14 @@
+const pkgName = package.name || basename;
+
+module.exports = {
+    name: prompt("package name", pkgName),
+    version: prompt("version", "0.0.0"),
+    description: prompt("description", ""),
+    main: prompt("entry point", "index.js"),
+    keywords: prompt((s) => { return s.split(/\s+/) }),
+    author: prompt("author", "Luis Float <contact@luisfloat.com> (https://luisfloat.com)"),
+    license: prompt("license", "UNLICENSED"),
+    repository: prompt("github repository url", `github:luisfloat/${pkgName}`),
+    homepage: prompt("homepage", `https://github.com/luisfloat/${pkgName}#readme`),
+    bugs: prompt("bugs", `https://github.com/luisfloat/${pkgName}/issues`),
+};
