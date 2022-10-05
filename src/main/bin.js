52f9aaa708214df3ef29d139e7cbf49fed268fce
Luis Float <contact@luisfloat.com>
2022-10-05 20:54:22 -0300

    refactor(main): move prompts into `prompts.js`

--- src/main/bin.js
+++ src/main/bin.js
@@ -2,65 +2,11 @@
 
 const colorette = require("colorette");
 const { cyan } = colorette;
-const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
 const { readPkg, writePkg } = require("./pkg.js");
-
-const basename = path.basename(process.cwd());
+const prompts = require("./prompts.js");
 
 const pkg = readPkg();
-const pkgName = pkg.name || basename;
-
-const prompts = {
-    name: {
-        message: "package name",
-        default: (s) => s || basename,
-    },
-    version: {
-        message: "version",
-        def: "0.0.0",
-    },
-    description: {
-        message: "description",
-    },
-    keywords: {
-        message: "keywords",
-        default: (s) => s && s.join(" "),
-        filter: (s) => s && s.split(/\s+/),
-    },
-    main: {
-        message: "entry point",
-        def: "index.js",
-    },
-    author: {
-        message: "author",
-        def: "Luis Float <contact@luisfloat.com> (https://luisfloat.com)",
-    },
-    license: {
-        message: "license",
-        def: "UNLICENSED",
-    },
-    repository: {
-        message: "repository",
-        def: `luisfloat/${pkgName}`,
-        default: (s) => s && `${new URL(s.url).pathname.replace("/", "").replace(".git", "")}`,
-        filter: (value) => value && {
-            type: "git",
-            url: `git+https://github.com/${value.replace("github:", "")}.git`,
-        },
-    },
-    homepage: {
-        message: "homepage",
-        def: `https://github.com/luisfloat/${pkgName}#readme`,
-    },
-    bugs: {
-        message: "bugs",
-        def: `https://github.com/luisfloat/${pkgName}/issues`,
-        default: (s) => s && s.url,
-        filter: (url) => url && { url },
-    },
-};
 
 const input = async (name, message, def) => (await inquirer.prompt({
     type: "input",
---
+++ src/main/prompts.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const { readPkg } = require("./pkg.js");
+
+const basename = path.basename(process.cwd());
+const pkg = readPkg();
+const pkgName = pkg.name || basename;
+
+const prompts = {
+    name: {
+        message: "package name",
+        default: (s) => s || basename,
+    },
+    version: {
+        message: "version",
+        def: "0.0.0",
+    },
+    description: {
+        message: "description",
+    },
+    keywords: {
+        message: "keywords",
+        default: (s) => s && s.join(" "),
+        filter: (s) => s && s.split(/\s+/),
+    },
+    main: {
+        message: "entry point",
+        def: "index.js",
+    },
+    author: {
+        message: "author",
+        def: "Luis Float <contact@luisfloat.com> (https://luisfloat.com)",
+    },
+    license: {
+        message: "license",
+        def: "UNLICENSED",
+    },
+    repository: {
+        message: "repository",
+        def: `luisfloat/${pkgName}`,
+        default: (s) => s && `${new URL(s.url).pathname.replace("/", "").replace(".git", "")}`,
+        filter: (value) => value && {
+            type: "git",
+            url: `git+https://github.com/${value.replace("github:", "")}.git`,
+        },
+    },
+    homepage: {
+        message: "homepage",
+        def: `https://github.com/luisfloat/${pkgName}#readme`,
+    },
+    bugs: {
+        message: "bugs",
+        def: `https://github.com/luisfloat/${pkgName}/issues`,
+        default: (s) => s && s.url,
+        filter: (url) => url && { url },
+    },
+};
+
+module.exports = prompts;
