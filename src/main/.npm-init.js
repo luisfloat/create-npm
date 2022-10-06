f99a29baf726a938d78297158cd7e41115b11ab2
Luis Float <contact@luisfloat.com>
2022-10-05 21:54:45 -0300

    refactor(main): gen init obj from `prompts.js`

--- src/main/.npm-init.js
+++ src/main/.npm-init.js
@@ -1,14 +1,14 @@
-const pkgName = package.name || basename;
+const prompts = require("./prompts");
 
-module.exports = {
-    name: prompt("package name", pkgName),
-    version: prompt("version", "0.0.0"),
-    description: prompt("description", ""),
-    main: prompt("entry point", "index.js"),
-    keywords: prompt((s) => { return s.split(/\s+/) }),
-    author: prompt("author", "Luis Float <contact@luisfloat.com> (https://luisfloat.com)"),
-    license: prompt("license", "UNLICENSED"),
-    repository: prompt("github repository url", `github:luisfloat/${pkgName}`),
-    homepage: prompt("homepage", `https://github.com/luisfloat/${pkgName}#readme`),
-    bugs: prompt("bugs", `https://github.com/luisfloat/${pkgName}/issues`),
-};
+function genInitObjFromPrompts() {
+    const init = {};
+    
+    for(let k in prompts) {
+        const p = prompts[k];
+        init[k] = prompt(p.message, p.def || p.default, p.filter);
+    }
+
+    return init;
+}
+
+module.exports = genInitObjFromPrompts();
